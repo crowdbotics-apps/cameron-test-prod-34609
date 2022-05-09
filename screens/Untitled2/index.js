2abbc72363572b2f15f74576f35cf9cc84d97ffe
cameron@crowdbotics.com <cameron@crowdbotics.com>
2022-05-09 09:31:20 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,5 @@
+import { Pressable } from "react-native";
+import { TextInput } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -8,16 +10,36 @@ const Untitled2 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      <View style={{
-      left: 31,
-      top: 45,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View></View>;
+      <Pressable onPress={{}} style={{
+      position: "initial",
+      width: "195px",
+      height: "350px",
+      marginLeft: "-10px",
+      marginTop: "-10px"
+    }}><View style={{
+        left: 31,
+        top: 45,
+        position: "absolute",
+        height: 60,
+        width: 140,
+        backgroundColor: "#E4E4E4",
+        borderRadius: 0,
+        color: "#777777"
+      }}></View></Pressable><Pressable onPress={{}} style={{
+      position: "initial",
+      width: "195px",
+      height: "350px",
+      marginLeft: "-10px",
+      marginTop: "-10px"
+    }}><TextInput style={{
+        left: 36,
+        top: 146,
+        position: "absolute",
+        backgroundColor: "#ffffff",
+        borderColor: "#cccccc",
+        width: 150,
+        height: 30
+      }}></TextInput></Pressable></View>;
 };
 
 export default Untitled2;
