80af0c2e9c32f005764422a73c268a003419d6e0
cameron@crowdbotics.com <cameron@crowdbotics.com>
2022-10-07 14:34:47 -0400

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,20 +1,15 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "cameron_test_prod_34609",
   url: "https://cameron_test_prod_34609.botics.co",
   api: "https://cameron_test_prod_34609.botics.co/api/v1"
-}
-
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   },
   "@modules/login": {
-    LOGO_URL:
-      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
-    BACKGROUND_URL:
-      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    LOGO_URL: "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL: "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
     HOME_SCREEN_NAME: "Home Screen",
     validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
     SignInNavText: "Login",
@@ -22,4 +17,4 @@ export const modulesOptions = {
     SignInButtonText: "Login",
     SignUpButtonText: "Sign Up"
   }
-}
+};
--- screens/Untitled9/index.js
+++ screens/Untitled9/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled9 = () => {
-    return (
-      <View style={{backgroundColor: '#f0f0f1', padding: 10, position: 'relative', height: '100%'}}>
-      </View>
-    )};
+  return <View style={_styles.SFTmWJBm}>
+      </View>;
+};
 
-  export default Untitled9;
+export default Untitled9;
+
+const _styles = StyleSheet.create({
+  SFTmWJBm: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
